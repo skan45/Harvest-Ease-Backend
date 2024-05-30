ce091b52278496e826a3a767b6040f9ffba4711d
mimii020 <imen.abidi@insat.ucar.tn>
2024-05-30 14:42:48 -0400

    chatbot works now

--- Db/dbconnection.js
+++ Db/dbconnection.js
@@ -1,13 +1,13 @@
-import mongoose from "mongoose";
+/* import mongoose from "mongoose";
 import dotenv from "dotenv";
-dotenv.config();
+dotenv.config(); */
 async function connectDB() {
-  try {
+/*   try {
     await mongoose.connect(process.env.DB_URI);
     console.log("Connected to MongoDB");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
     throw error;
-  }
+  } */
 }
 export default connectDB;
